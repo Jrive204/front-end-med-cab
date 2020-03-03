a88209ff09b193bbb0269325d77b06eb6eadf0c2
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-03-03 11:49:22 -0700

    updated styling and functionality of login page

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,10 +1,104 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { UserLogin } from '../Actions/ActionCreator';
+import { ReactSVG } from 'react-svg';
 import styled from 'styled-components';
-// import logo from '../img/logo.png';
 import { axiosWithAuth } from "../Utils/axiosWithAuth";
+
+const Container = styled.div`
+    width:100%;
+    height:100vh;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    background-color:#98FB98;
+`
+
+const LoginWindow = styled.div`
+    border:1px solid #F5F5F5;
+    border-radius:5px;
+    width:25%;
+    min-width:300px;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    background-color:white;
+    padding:20px;
+    & form {
+        width:100%;
+    }
+`
+
+const Heading = styled.div`
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    h1 {
+        font-family: 'Lilita One', sans-serif;
+        font-size:200%;
+        font-weight:bold;  
+    }
+`
+
+const InputContainer = styled.div`
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+    width:100%;
+    margin: 5px 0;
+`
+
+const Label = styled.label`
+    width:40%;
+`
+
+const Input = styled.input`
+    width:60%;
+    background-color:#F5F5F5;
+    border-radius:3px;
+    padding:8px;
+    outline:none;
+    font-size:80%;
+    font-family:inherit;
+    border:1px solid white;
+    &:hover {
+        border:1px solid cornflowerblue;
+    }
+`
+
+const Validation = styled.div`
+    width:100%;
+    height:25px;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    & > div {
+        display:none;
+        color:red;
+    }
+`
+
+const Submit = styled.button`
+    width:100%;
+    font-size:110%;
+    padding:8px;
+    background-color:#3CB371;
+    color:white;
+    border-radius:5px;
+    &:hover {
+        cursor:pointer;
+        border:1px solid #98FB98;
+        color:#98FB98;
+    }
+    &:active {
+        background-color:#2E8B57;
+        outline:none;
+    }
+    &:focus {
+        outline:none;
+    }
+`
 
 
 const SignIn = props => {
@@ -12,24 +106,36 @@ const SignIn = props => {
       email:"",
       password: ""
     });
+    const [match, setMatchStatus] = useState(true);
+    const [emptyValues, setValueStatus] = useState(false);
   
     const handleSubmit = evt => {
       evt.preventDefault();
-      const userCredentials = {
-        username: values.email,
-        password: values.password
-      };
-      //wasnt able to get props.history to work inside of an action
-      axiosWithAuth().post('https://medcabinet1.herokuapp.com/api/auth/login', userCredentials)
-  
-      .then(response =>{
-        localStorage.setItem("token", response.data.token);
-        localStorage.setItem("user_id", response.data.id);
-        props.history.push("/dashboard")
-        console.log(response.data)
+      let emptyInputs = false;
+      Object.values(values).forEach(value => {
+          if (value === "") {
+              emptyInputs = true;
+          }
       })
-      .catch(err => console.log(err.response))
-      
+      if (emptyInputs) {
+          setValueStatus(true);
+      }
+      else {
+        const userCredentials = {
+          username: values.email,
+          password: values.password
+        };
+        //wasnt able to get props.history to work inside of an action
+        axiosWithAuth().post('https://medcabinet1.herokuapp.com/api/auth/login', userCredentials)
+    
+        .then(response =>{
+          localStorage.setItem("token", response.data.token);
+          localStorage.setItem("user_id", response.data.id);
+          props.history.push("/dashboard")
+          console.log(response.data)
+        })
+        .catch(err => console.log(err.response))
+      }
     };
     const handleChange = (evt) => {
       evt.preventDefault();
@@ -37,30 +143,36 @@ const SignIn = props => {
         ...values,
         [evt.target.name]: evt.target.value
       })
+      setValueStatus(false);
+      console.log(values);
     }
   
     return (
-      <div>
-        {/* <img src={logo} alt='bestbud logo'/> */}
-        <h2>Sign In</h2>
-  
-        <form onSubmit={handleSubmit}>
-          <label>Username</label>
-          <input type='text' name='email' placeholder='Enter a username' value={values.email} onChange={handleChange}/>
-  
-          <label>Password</label>
-          <input type='password' name='password' placeholder='Enter a password' value={values.password} onChange={handleChange}/>
-  
-          <button type='submit'>Sign In</button>
-        </form>
-  
-        <Link to='/signup'>Dont have an account? Sign up here.</Link>
-      </div>
+      <Container>
+        <LoginWindow>
+          <Heading><ReactSVG src="flask.svg"/><h1>Login</h1></Heading>
+          <form onSubmit={handleSubmit}>
+              <InputContainer>
+                  <Label htmlFor="username">Username</Label>
+                  <Input type="text" id="username" name="username" style={emptyValues ? {border: "1px solid red"} : {border: "1px solid white"}} value={values.username} onChange={handleChange}/>
+              </InputContainer>
+              <InputContainer>
+                  <Label htmlFor="password">Password</Label>
+                  <Input type="password" id="password" name="password" style={emptyValues ? {border: "1px solid red"} : {border: "1px solid white"}} value={values.password} onChange={handleChange}/>
+              </InputContainer>
+              <Validation>
+                  <div style={match ? {display:"none"} : {display:"block"}}>Could not find user</div>
+              </Validation>
+              <Submit type="submit">Log In</Submit>
+          </form>
+          <NavLink style={{marginTop: "10px"}} to="/signup">Don't have an account? Sign up here!</NavLink>
+        </LoginWindow>
+      </Container>
     );
   };
   
   const mapStateToProps = state => {
-    return{
+    return {
       user: state.email,
       passsword: state.password
     }
