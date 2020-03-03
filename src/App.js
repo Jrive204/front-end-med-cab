ab94605def684713e795835b3447a01bd8c2f43b
unknown <g.hawman@gmail.comgit config --global user.name Gregory>
2020-03-03 10:35:40 -0500

    got the sign up form, and styled it like the login in form.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignUpPage from './components/SignUpPage'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <SignUpPage />
+    </div>  
+    
+  );
+}
+
+export default App;
