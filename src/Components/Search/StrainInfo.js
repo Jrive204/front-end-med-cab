4b38a9a65d96e3e95e5971d6b0c11da1e1cfc56e
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-03-05 10:07:00 -0700

    click on strain card for more information

--- src/Components/Search/StrainInfo.js
+++ src/Components/Search/StrainInfo.js
@@ -6,9 +6,11 @@ import styled from 'styled-components'
 const Card = styled.div`
     background-color:white;
     border-radius:10px;
-    margin:10%;
+    margin:5%;
     display:flex;
     flex-direction:column;
+    font-size:120%;
+    border: 1px solid #F5F5F5;
     > .heading {
         display:flex;
         justify-content:center;
@@ -35,9 +37,30 @@ const Card = styled.div`
             width:98%;
         }
     }
+    > div:last-child {
+        padding:20px;
+        .id-race-rating {
+            display:flex;
+            justify-content:space-between;
+            font-weight:bold;
+        }
+        .description {
+            text-align:center;
+            margin:10px;
+        }
+        .effects {
+            margin:10px;
+            display:flex;
+            align-items:center;
+            flex-direction:column;
+            > p:first-child {
+                font-weight:bold;
+            }
+        }
+    }
     h1 {
         font-weight:bold;
-        font-size:200%;
+        font-size:150%;
     }
 `
 
@@ -45,7 +68,26 @@ const StrainInfoCard = ({data}) => {
     return (
         <Card>
             <div className="heading"><div><Link to="/strains">{`<`}</Link></div><div><ReactSVG src={`${data.race}.svg`} /><h1>{data.name}</h1></div></div>
-            <div><p>ID: {data.id}</p><p>{data.race}</p><p>Rating: {data.strain_rating}</p></div>
+            <div>
+                <div class="id-race-rating"><p>ID: {data.id}</p><p className="race">{data.race}</p><p>Rating: {data.strain_rating}</p></div>
+                <div class="description">{data.strain_description}</div>
+                <div class="effects">
+                    <p>Flavors</p>
+                    <p>{data.flavors}</p>
+                </div>
+                <div class="effects">
+                    <p>Positive Effects</p>
+                    <p>{data.positive}</p>
+                </div>
+                <div class="effects">
+                    <p>Negative Effects</p>
+                    <p>{data.negative}</p>
+                </div>
+                <div class="effects">
+                    <p>Helpful for those who are experiencing:</p>
+                    <p>{data.medical}</p>
+                </div>
+            </div>
         </Card>
     )
 }
